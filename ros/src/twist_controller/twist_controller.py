2fec1302a7b09fb698ec042aade7a5a494d1b167
roy-wang-py <wpyroy@qq.com>
2019-01-23 06:10:34 -0800

    fix:PID control manu tune pid parms.     by wangpeiyao<wpyroy@qq.com>

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -13,16 +13,20 @@ class Controller(object):
         # TODO: Implement
         self.yaw_controller = YawController(wheel_base, steer_ratio, 0.1, max_lat_accel, max_steer_angle)
         
-        kp = 0.3
-        ki = 0.1
-        kd = 0.
-        mn = 0.
-        mx = 0.2
+        kp = 5
+        ki = 0.5
+        kd = 0.5
+        #mn = 0.
+        #mx = 0.2
+        mn = decel_limit
+        mx = accel_limit
         self.throttle_controller = PID(kp,ki,kd,mn,mx)
         
-        tau = 0.5
-        ts = .02
+        tau = 3
+        ts = 1
         self.vel_lpf = LowPassFilter(tau,ts)
+        
+        self.steer_lpf = LowPassFilter(tau = 3, ts = 1)
         
         self.vehicle_mass = vehicle_mass
         self.fuel_capacity = fuel_capacity
@@ -42,18 +46,19 @@ class Controller(object):
             self.throttle_controller.reset()
             return 0., 0., 0,
             
-        current_vel = self.vel_lpf.filt(current_vel)
-        
-        steering = self.yaw_controller.get_steering(linear_vel, angular_vel, current_vel)
-        
         vel_error = linear_vel - current_vel
         self.last_vel = current_vel
         
         current_time = rospy.get_time()
-        sample_time = current_time = self.last_time
-        self.last_time = current_time
+        sample_time = current_time - self.last_time
+        self.last_time = current_time     
+        
+        steering = self.yaw_controller.get_steering(linear_vel, angular_vel, current_vel)
+        steering = self.steer_lpf.filt(steering)
         
         throttle = self.throttle_controller.step(vel_error, sample_time)
+        throttle = self.vel_lpf.filt(throttle)
+
         brake = 0
         
         if linear_vel == 0. and current_vel < 0.1:
@@ -62,7 +67,7 @@ class Controller(object):
         elif throttle < .1 and vel_error < 0:
             throttle = 0
             decel = max(vel_error, self.decel_limit)
-            brake = abs(decel) * self.vehicle_mass*self.wheel_radius
+            brake = abs(decel) * (self.vehicle_mass + self.fuel_capacity * GAS_DENSITY)*self.wheel_radius
             
         return throttle, brake, steering
 
